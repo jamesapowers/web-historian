7424c2f4c79eaa80559aa62902627d40211c6933
jamesapowers <powersajames@gmail.com>
2018-02-07 19:59:08 -0800

    partially completed archive-helpers tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,53 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  return fs.readFile(this.paths.list, 'utf8', function(err, data) {
+    if (err) {
+      throw (err);
+    }
+    var arr = data.toString().split('\n');
+    return callback(arr);
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  return exports.readListOfUrls(function(data) {
+    return callback(data.includes(url));
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(this.paths.list, url, 'utf8', function(err) {
+    if (err) {
+      throw err;
+    }
+    callback();
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  // fs.readFile(this.path.archivedSites, 'utf8', function(err, data) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   console.log(data);
+  // });
+
+  // var archivedSites = this.paths.archivedSites;
+  // for (var i = 0; i < archivedSites.length; i++) {
+  //   if (callback(archivedSites[i]).url === url) {
+  //     return true;
+  //   }
+  // }
+  // return false;
 };
 
 exports.downloadUrls = function(urls) {
+  // for (var i = 0; i < urls.length; i++) {
+  //   if (this.isUrlArchived(urls[i], function (url) {
+  //     JSON.parse(url);
+  //   })) {
+  //     return urls[i];
+  //   }
+  // }
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -4,4 +4,21 @@ var archive = require('../helpers/archive-helpers');
 
 exports.handleRequest = function (req, res) {
   res.end(archive.paths.list);
+  
+  if (req.method === 'GET') {
+    // const inputValue = document.getElementById('inputbox').val();
+    // console.log(inputValue)
+    
+    console.log(res.statusCode);
+    archive.readListOfUrls( (data) => {
+      res.statusCode = 200;
+      let string = data.join(',');
+      console.log(string);
+      res.end(string); 
+    });
+  }
+
+  if (req.method === 'POST') {
+  
+  }
 };
